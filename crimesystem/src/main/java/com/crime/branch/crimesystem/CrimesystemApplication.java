23f7f0eeba904c82e6d441a0bd13ea29554b90de
Lucifer <tridibmondal>
2022-01-09 21:38:03 +0530

    commit done

---
+++ crimesystem/src/main/java/com/crime/branch/crimesystem/CrimesystemApplication.java
@@ -0,0 +1,13 @@
+package com.crime.branch.crimesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrimesystemApplication.class, args);
+	}
+
+}
